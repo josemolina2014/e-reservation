1d43e1d169b10883689b85cc33ad2bf3de85e7a6
jose molina <josmoli@gmail.com>
2020-02-22 21:38:10 -0500

    Optimizar Pojos con Lombok

---
+++ src/main/java/com/platzi/ereservation/model/Client.java
@@ -0,0 +1,22 @@
+package com.platzi.ereservation.model;
+
+import lombok.Data;
+
+/**
+ * Client table
+ * @author josemolina
+ *
+ */
+@Data
+public class Client {
+	
+	private String  cliName;
+	private String  cliLastName;
+	
+	public Client() {
+		
+	}
+
+	
+
+}
