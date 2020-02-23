746db2c5a2c228237410c3cf57e537a7d0e08221
jose molina <josmoli@gmail.com>
2020-02-23 18:39:56 -0500

    Características de los Servicios Web y Spring Rest

---
+++ src/main/java/com/platzi/ereservation/view/resources/ClientResource.java
@@ -0,0 +1,17 @@
+package com.platzi.ereservation.view.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Cliente de servicio web
+ * @author josemolina
+ *
+ */
+
+@RestController
+@RequestMapping("/api/client")
+public class ClientResource {
+	
+
+}
