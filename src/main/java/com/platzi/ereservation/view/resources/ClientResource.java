8fdb1c629528894af80df9079c0ce5e58ce83437
jose molina <josmoli@gmail.com>
2020-02-23 19:40:30 -0500

    REST para crear y actualizar información

--- src/main/java/com/platzi/ereservation/view/resources/ClientResource.java
+++ src/main/java/com/platzi/ereservation/view/resources/ClientResource.java
@@ -1,7 +1,17 @@
 package com.platzi.ereservation.view.resources;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.platzi.ereservation.business.services.ClientService;
+import com.platzi.ereservation.model.Client;
+import com.platzi.ereservation.view.resources.vo.ClientVO;
 
 /**
  * Cliente de servicio web
@@ -13,5 +23,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/client")
 public class ClientResource {
 	
+	private final ClientService clientService;
+
+	public ClientResource(ClientService clientService) {
+		this.clientService = clientService;
+	}
+	
+	@PostMapping
+	public ResponseEntity<Client> createClient(@RequestBody ClientVO clientVO){
+		Client client = new Client();
+		
+		client.setCliId(clientVO.getCliId());	
+		client.setCliName(clientVO.getCliName());
+		client.setCliLastName(clientVO.getCliLastName());	
+		client.setCliAddress(clientVO.getCliAddress());
+		client.setCliPhone(clientVO.getCliPhone());
+		client.setCliEmail(clientVO.getCliEmail());
+		client.setCliIdentification(clientVO.getCliIdentification());
+		
+		return new ResponseEntity<>(this.clientService.create(client), HttpStatus.CREATED);
+	}
+	
+	
+	@PutMapping("/{identification}")
+	public ResponseEntity<Client> updateClient(@PathVariable("identification") String identification,  @RequestBody ClientVO clientVO){
+		Client client = this.clientService.findByIdentification(identification);
+		if(client==null)
+		{
+			return new ResponseEntity<Client>(HttpStatus.NOT_FOUND);
+		}
+		else {
+			client.setCliId(clientVO.getCliId());	
+			client.setCliName(clientVO.getCliName());
+			client.setCliLastName(clientVO.getCliLastName());	
+			client.setCliAddress(clientVO.getCliAddress());
+			client.setCliPhone(clientVO.getCliPhone());
+			client.setCliEmail(clientVO.getCliEmail());			
+		}
+		return new ResponseEntity<>(this.clientService.update(client), HttpStatus.OK);
+		
+	}
 
 }
---
+++ src/main/java/com/platzi/ereservation/view/resources/vo/ClientVO.java
@@ -0,0 +1,15 @@
+package com.platzi.ereservation.view.resources.vo;
+
+import lombok.Data;
+
+@Data
+public class ClientVO {
+
+	private String cliId;	
+	private String cliName;
+	private String cliLastName;	
+	private String cliAddress;
+	private String cliPhone;
+	private String cliEmail;
+	private String cliIdentification;
+}
